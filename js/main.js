f20421107dedbf3a1e9a646dfcd023a53f922b9f
local_ubuntu <kanikama775@gmail.com>
2023-05-14 23:14:39 +0900

    :tada: タッチイベントに対応

--- js/main.js
+++ js/main.js
@@ -7,12 +7,24 @@ import { PushPuchi, clickStyle } from "./action.js";
   // 初期表示用のプチプチ作成
   CreatePuchi("firstPuchi");
 
+  // デバイス判定
+  if ("ontouchstart" in window) {
+    // タッチデバイスの場合
+    const device = "touch";
+    // 初期のプチプチクリックイベント設定
+    PushPuchi(device);
+    // 単発-連射モード切り替え設定
+    clickStyle(device);
+  } else {
+    // タッチデバイスではない場合
+    const device = "mouse";
+    // 初期のプチプチクリックイベント設定
+    PushPuchi(device);
+    // 単発-連射モード切り替え設定
+    clickStyle(device);
+  }
+
   // スクロール時のプチプチ作成
+
   scrollPages();
-
-  // 初期のプチプチクリックイベント設定
-  PushPuchi();
-
-  // 単発-連射モード切り替え設定
-  clickStyle();
 }
