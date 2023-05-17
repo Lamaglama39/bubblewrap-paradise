2ff13f7ab36e686f8f610f4a5e293b718f0a6c77
local_ubuntu <kanikama775@gmail.com>
2023-05-17 21:19:06 +0900

    :+1: レスポンスに関する修正       -不要音声ファイルの削除       -背景画像をlinear-gradientに入れ替え

--- js/sound.js
+++ js/sound.js
@@ -1,31 +1,14 @@
 // プチプチ音一覧
-let pop = [
-  "Packaging_Air_Cushion01-07(Pop).mp3",
-  "Packaging_Air_Cushion01-08(Pop).mp3",
-  // 'Packaging_Air_Cushion01-09(Pop).mp3',
-  // 'Packaging_Air_Cushion01-10(Pop).mp3',
-  "Packaging_Air_Cushion01-17(Far-Pop).mp3",
-  "Packaging_Air_Cushion01-18(Far-Pop).mp3",
-  // 'Packaging_Air_Cushion01-19(Far-Pop).mp3',
-  // 'Packaging_Air_Cushion01-20(Far-Pop).mp3',
-];
-
-let rustle = [
-  "Packaging_Air_Cushion01-01(Rustle).mp3",
-  "Packaging_Air_Cushion01-02(Rustle).mp3",
-  "Packaging_Air_Cushion01-03(Rustle).mp3",
-  "Packaging_Air_Cushion01-04(Rustle).mp3",
-  "Packaging_Air_Cushion01-05(Rustle).mp3",
-  "Packaging_Air_Cushion01-11(Far-Rustle).mp3",
-  "Packaging_Air_Cushion01-12(Far-Rustle).mp3",
-  "Packaging_Air_Cushion01-13(Far-Rustle).mp3",
-  "Packaging_Air_Cushion01-14(Far-Rustle).mp3",
-  "Packaging_Air_Cushion01-15(Far-Rustle).mp3",
-  "Packaging_Air_Cushion01-16(Far-Rustle).mp3",
-];
 const soundPath = "../src/sound/";
 
 // クリック時の音
+let pop = [
+  "Packaging_Air_Cushion01-07(Pop).mp3",
+  "Packaging_Air_Cushion01-08(Pop).mp3",
+  "Packaging_Air_Cushion01-17(Far-Pop).mp3",
+  "Packaging_Air_Cushion01-18(Far-Pop).mp3",
+];
+
 async function popSound() {
   let rand = Math.floor(Math.random() * pop.length);
   let sound = soundPath + pop[rand];
@@ -35,6 +18,12 @@ async function popSound() {
 }
 
 // スクロール時の音
+let rustle = [
+  "Packaging_Air_Cushion01-01(Rustle).mp3",
+  "Packaging_Air_Cushion01-02(Rustle).mp3",
+  "Packaging_Air_Cushion01-03(Rustle).mp3",
+];
+
 async function rustleSound() {
   let rand = Math.floor(Math.random() * rustle.length);
   let sound = soundPath + rustle[rand];
