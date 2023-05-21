a635492a50ca78975abe574d537a9c9c85267455
Lamaglama39 <lamaglama621@gmail.com>
2023-05-21 22:51:30 +0900

    :bug: タッチデバイスの場合、スワイプでイベントが発生しない問題を修正

--- js/action.js
+++ js/action.js
@@ -1,60 +1,61 @@
 import { popSound } from "./sound.js";
 
-// プチプチ状態判定
-function handleClick(e, eventType) {
-  let circle = e.target;
-  if (circle.classList.contains("alive")) {
-    popSound();
-    change(circle);
-    addCount();
-  }
-  if (eventType === "touchmove") {
-    circle.removeEventListener("touchmove", handleClick, true);
-  } else {
-    circle.removeEventListener("mousemove", handleClick, true);
-  }
-}
-
-// プチプチ時のクリックイベント
-async function PushPuchi(device) {
-  const eventTypes =
-    device === "touch" ? ["touchstart", "touchmove"] : ["click", "mousemove"];
+// ぷちぷちクリックイベント設定
+function PushPuchi(device) {
+  const eventTypes = device === "touch" ? ["touchstart", "touchmove"] : ["click", "mousemove"];
   let circles = Array.from(document.querySelectorAll(".alive"));
 
   circles.forEach((circle) => {
     let mode = document.getElementById("modeButton");
-    const handleClickWrapper = (e) =>
-      handleClick(
-        e,
-        mode.classList.contains("normal") ? eventTypes[0] : eventTypes[1]
-      );
-    circle.addEventListener(
-      mode.classList.contains("normal") ? eventTypes[0] : eventTypes[1],
-      handleClickWrapper,
-      true
-    );
-    circle.handleClickWrapper = handleClickWrapper; // 保持するために新しいプロパティに関数を保存
+    const handleClickWrapper = (e) => handleClick(e, mode.classList.contains("normal") ? eventTypes[0] : eventTypes[1]);
+    circle.addEventListener(mode.classList.contains("normal") ? eventTypes[0] : eventTypes[1], handleClickWrapper, true);
+    circle.handleClickWrapper = handleClickWrapper;
   });
 }
 
-// プチプチ画像置き換え
-async function change(circle) {
-  circle.src = "src/img/background.png";
-  circle.classList.remove("alive");
+// ぷちぷちクリック判定
+function handleClick(e, eventType) {
+  performActionOnElement(e.target);
+  if (eventType === "touchmove") {
+    e.preventDefault();
+    document.addEventListener('touchmove', function(event) {
+      let touch = event.touches[0];
+      let element = document.elementFromPoint(touch.clientX, touch.clientY);
+      performActionOnElement(element);
+    }, false);
+    e.target.removeEventListener("touchmove", e.target.handleClickWrapper, true);
+  } else {
+    e.target.removeEventListener("mousemove", e.target.handleClickWrapper, true);
+  }
 }
 
-// スコアカウント
+// 共通イベント
+function performActionOnElement(element) {
+  if (element && element.tagName.toLowerCase() === 'img' && element.classList.contains("alive")) {
+    popSound();
+    changeImage(element);
+    addCount();
+  }
+}
+
+// ぷちぷち画像変更
+function changeImage(circle) {
+  circle.src = "src/img/background.png";
+  circle.classList.remove("alive");
+  circle.classList.add("after");
+}
+
+// スコアカウンター
 let count = 0;
-async function addCount() {
+function addCount() {
   let countElement = document.getElementById("Counter");
   count++;
   countElement.textContent = count.toString();
 }
 
 // 単発-連射モード切り替え
-async function clickStyle(device) {
-  const eventTypes =
-    device === "touch" ? ["touchstart", "touchmove"] : ["click", "mousemove"];
+function clickStyle(device) {
+  const eventTypes = device === "touch" ? ["touchstart", "touchmove"] : ["click", "mousemove"];
   let modeButton = document.getElementById("modeButton");
   let mode = document.getElementById("mode");
 
@@ -67,11 +68,8 @@ async function clickStyle(device) {
       mode.innerHTML = "auto";
 
       circles.forEach((circle) => {
-        circle.removeEventListener(
-          eventTypes[0],
-          (e) => handleClick(e, eventTypes[0]),
-          true
-        );
+        circle.removeEventListener(eventTypes[0], circle.handleClickWrapper, true);
+        circle.handleClickWrapper = null;
       });
     } else {
       modeButton.classList.remove("auto");
@@ -79,22 +77,14 @@ async function clickStyle(device) {
       mode.innerHTML = "normal";
 
       circles.forEach((circle) => {
-        if (modeButton.classList.contains("normal")) {
-          circle.removeEventListener(
-            eventTypes[1],
-            circle.handleClickWrapper,
-            true
-          ); // 以前に保存した関数を使用
-        } else {
-          circle.removeEventListener(
-            eventTypes[0],
-            circle.handleClickWrapper,
-            true
-          ); // 以前に保存した関数を使用
+        if (circle.handleClickWrapper) {
+          circle.removeEventListener(eventTypes[1], circle.handleClickWrapper, true);
+          circle.handleClickWrapper = null;
         }
       });
     }
     PushPuchi(device);
   });
 }
+
 export { PushPuchi, clickStyle };
